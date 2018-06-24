6ea6eaff848f194ea2f2f454a6f1d3aa2da49b2b
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-24 23:39:45 +0530

    Book Publisher !

---
+++ BookManagement/src/com/project/bookmanagement/entity/Publisher.java
@@ -0,0 +1,128 @@
+package com.project.bookmanagement.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Publisher implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5148523848088702448L;
+	private Integer publisherId;
+	private String publisherName;
+	private String publisherAddress;
+	private String publisherPhone;
+	private List<Book> books;
+	/**
+	 * @return the publisherId
+	 */
+	public Integer getPublisherId() {
+		return publisherId;
+	}
+	/**
+	 * @param publisherId the publisherId to set
+	 */
+	public void setPublisherId(Integer publisherId) {
+		this.publisherId = publisherId;
+	}
+	/**
+	 * @return the publisherName
+	 */
+	public String getPublisherName() {
+		return publisherName;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result
+				+ ((publisherAddress == null) ? 0 : publisherAddress.hashCode());
+		result = prime * result
+				+ ((publisherId == null) ? 0 : publisherId.hashCode());
+		result = prime * result
+				+ ((publisherName == null) ? 0 : publisherName.hashCode());
+		result = prime * result
+				+ ((publisherPhone == null) ? 0 : publisherPhone.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Publisher other = (Publisher) obj;
+		if (publisherAddress == null) {
+			if (other.publisherAddress != null)
+				return false;
+		} else if (!publisherAddress.equals(other.publisherAddress))
+			return false;
+		if (publisherId == null) {
+			if (other.publisherId != null)
+				return false;
+		} else if (!publisherId.equals(other.publisherId))
+			return false;
+		if (publisherName == null) {
+			if (other.publisherName != null)
+				return false;
+		} else if (!publisherName.equals(other.publisherName))
+			return false;
+		if (publisherPhone == null) {
+			if (other.publisherPhone != null)
+				return false;
+		} else if (!publisherPhone.equals(other.publisherPhone))
+			return false;
+		return true;
+	}
+	/**
+	 * @param publisherName the publisherName to set
+	 */
+	public void setPublisherName(String publisherName) {
+		this.publisherName = publisherName;
+	}
+	/**
+	 * @return the publisherAddress
+	 */
+	public String getPublisherAddress() {
+		return publisherAddress;
+	}
+	/**
+	 * @param publisherAddress the publisherAddress to set
+	 */
+	public void setPublisherAddress(String publisherAddress) {
+		this.publisherAddress = publisherAddress;
+	}
+	/**
+	 * @return the publisherPhone
+	 */
+	public String getPublisherPhone() {
+		return publisherPhone;
+	}
+	/**
+	 * @param publisherPhone the publisherPhone to set
+	 */
+	public void setPublisherPhone(String publisherPhone) {
+		this.publisherPhone = publisherPhone;
+	}
+	/**
+	 * @return the books
+	 */
+	public List<Book> getBooks() {
+		return books;
+	}
+	/**
+	 * @param books the books to set
+	 */
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+}
