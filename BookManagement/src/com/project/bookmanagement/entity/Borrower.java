9155e15cea58ac8f34a661148a8ff0f166e8e673
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-24 23:42:03 +0530

    Book Borrower !

---
+++ BookManagement/src/com/project/bookmanagement/entity/Borrower.java
@@ -0,0 +1,109 @@
+package com.project.bookmanagement.entity;
+
+import java.io.Serializable;
+
+public class Borrower implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9200732818410396517L;
+	public Integer cardNo;
+	public String name;
+	public String address;
+	public String phone;
+	/**
+	 * @return the cardNo
+	 */
+	public Integer getCardNo() {
+		return cardNo;
+	}
+	/**
+	 * @param cardNo the cardNo to set
+	 */
+	public void setCardNo(Integer cardNo) {
+		this.cardNo = cardNo;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((cardNo == null) ? 0 : cardNo.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Borrower other = (Borrower) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (cardNo == null) {
+			if (other.cardNo != null)
+				return false;
+		} else if (!cardNo.equals(other.cardNo))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		return true;
+	}
+}
