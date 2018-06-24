623ff4d533ca09fc811160c16a836d5dcddf0803
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-24 23:46:48 +0530

    Book !

---
+++ BookManagement/src/com/project/bookmanagement/entity/Book.java
@@ -0,0 +1,112 @@
+package com.project.bookmanagement.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Book implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -625851397204514719L;
+	private Integer bookId;
+	private String title;
+	private Publisher publisher;
+	private List<Author> authors;
+	private List<Genre> genres;
+	/**
+	 * @return the genres
+	 */
+	public List<Genre> getGenres() {
+		return genres;
+	}
+	/**
+	 * @param genres the genres to set
+	 */
+	public void setGenres(List<Genre> genres) {
+		this.genres = genres;
+	}
+	/**
+	 * @return the bookId
+	 */
+	public Integer getBookId() {
+		return bookId;
+	}
+	/**
+	 * @param bookId the bookId to set
+	 */
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the publisher
+	 */
+	public Publisher getPublisher() {
+		return publisher;
+	}
+	/**
+	 * @param publisher the publisher to set
+	 */
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
+	/**
+	 * @return the authors
+	 */
+	public List<Author> getAuthors() {
+		return authors;
+	}
+	/**
+	 * @param authors the authors to set
+	 */
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bookId == null) ? 0 : bookId.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (bookId == null) {
+			if (other.bookId != null)
+				return false;
+		} else if (!bookId.equals(other.bookId))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+}
