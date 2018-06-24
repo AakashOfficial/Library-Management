6a91040fcc46206d42f06aeadad5be8f1481a734
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-24 23:40:24 +0530

    Book Author !

---
+++ BookManagement/src/com/project/bookmanagement/entity/Author.java
@@ -0,0 +1,66 @@
+package com.project.bookmanagement.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+public class Author implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9151170513668626160L;
+	private Integer authorId;
+	private String authorName;
+	private List<Book> books;
+	public Integer getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(Integer authorId) {
+		this.authorId = authorId;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public List<Book> getBooks() {
+		return books;
+	}
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((authorId == null) ? 0 : authorId.hashCode());
+		result = prime * result
+				+ ((authorName == null) ? 0 : authorName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (authorId == null) {
+			if (other.authorId != null)
+				return false;
+		} else if (!authorId.equals(other.authorId))
+			return false;
+		if (authorName == null) {
+			if (other.authorName != null)
+				return false;
+		} else if (!authorName.equals(other.authorName))
+			return false;
+		return true;
+	}
+	
+	
+}
